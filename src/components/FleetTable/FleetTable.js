672a652a863dbec1d0c89abf2f1f3876d3358897
TheSnakeRekt <jorgeradasilva@gmail.com>
2022-11-03 12:28:23 +0000

    feat(assignment-3): Lists and Keys

--- src/components/FleetTable/FleetTable.js
+++ src/components/FleetTable/FleetTable.js
@@ -1,27 +1,26 @@
-import './FleetTable.css'
+import "./FleetTable.css";
 import FleetAtom from "../../atoms/FleetAtom";
 
-
 const FleetTable = (props) => {
-    return (
-        <table>
-            <thead>
-                <tr>                
-                    <th>License Plate</th>
-                    <th>Brand</th>
-                    <th>Model & Year</th>
-                    <th>Mileage</th>
-                    <th>Driver</th>
-                    <th>Route</th>
-                </tr>
-            </thead>
-            <tbody>
-                    {
-                        props.data.map(truck => <FleetAtom vehicle={truck} key={truck.id}/>)
-                    }
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>License Plate</th>
+          <th>Brand</th>
+          <th>Model & Year</th>
+          <th>Mileage</th>
+          <th>Driver</th>
+          <th>Route</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((truck) => (
+          <FleetAtom vehicle={truck} key={truck.id} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default FleetTable;
